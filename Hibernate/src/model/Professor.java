79d77217d54e00490ae582756a17bc5cf02815d5
heber <heber@buntu>
2017-05-26 17:46:08 +0200

    hola que tal

---
+++ Hibernate/src/model/Professor.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.io.Serializable;
+
+public class Professor implements Serializable{
+	static final long serialVersionUID = 1L;
+	private int id;
+	private String nom;
+	private String cognom1;
+	private String cognom2;
+	
+	public Professor(){
+		
+	}
+	
+	public Professor(int id, String nom, String cognom1, String cognom2) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.cognom1 = cognom1;
+		this.cognom2 = cognom2;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getCognom1() {
+		return cognom1;
+	}
+
+	public void setCognom1(String cognom1) {
+		this.cognom1 = cognom1;
+	}
+
+	public String getCognom2() {
+		return cognom2;
+	}
+
+	public void setCognom2(String cognom2) {
+		this.cognom2 = cognom2;
+	}
+	
+}
